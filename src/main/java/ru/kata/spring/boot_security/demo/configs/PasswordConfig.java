4644d9e7a01cd7dad970b816e42d1a9dd4d00aa1
AlexeySolomatin <mr.alex.sol@yandex.ru>
2022-10-21 16:01:33 +0300

    !!!!!

--- src/main/java/ru/kata/spring/boot_security/demo/configs/PasswordConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/PasswordConfig.java
@@ -3,12 +3,13 @@ package ru.kata.spring.boot_security.demo.configs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class PasswordConfig {
     @Bean
     public PasswordEncoder getPasswordEncoder() {
-        return new BCryptPasswordEncoder();
+        return NoOpPasswordEncoder.getInstance();
     }
 }
